a0c7ed9e40216b7e185608ad66c8d8cad057e493
RenanBortolato <renanbbcordeiro@gmail.com>
2025-03-18 08:28:44 -0300

    componente

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,28 @@ import { useState } from "react"
 function App(){
 
   const [nome, setNome] = useState('ronaldo');
-  return (
-    <div>
-      <h3>Pizarria 2A</h3>
-    <input className="nome"
-    name ="nome"
-    placeholder="digite se nome..."
-    onChange={(e)=>setNome(e.target.value)}
-    type="text"/>
+
+  const Formulario = () => {
+    return (
+      <>
+      <h4>Formulario de inscrição</h4>
+   <input className="nome"
+       name ="nome"
+       placeholder="digite se nome..."
+       onChange={(e)=>setNome(e.target.value)}
+       type="text"/>
     <button onClick={()=>{alert(nome)}}>
      CLIQUE AQUI
      </button>
+</>
+    )
+  }
+  return (
+    <div>
+      <h3>Pizarria 2A</h3>
+    <Formulario/>
+    <Formulario/>
+    <Formulario/>
     </div>
   )
 }
